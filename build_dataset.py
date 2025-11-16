cd8f8402702d9c05af6c721f306bda587fe6ab72
tom99 <jonbo99763@gmail.com>
2025-11-16 11:46:45 +0800

    update

--- build_dataset.py
+++ build_dataset.py
@@ -1,28 +1,34 @@
-import os
 import pandas as pd
+import os
 from tqdm import tqdm
+from pathlib import Path
+import numpy as np
+import tifffile
 from src.procs.proc_data import *
 
 data_path = './data/vesuvius-challenge-surface-detection'
 
-
 def main():
     base_dir = Path(data_path)
-    meta_data = pd.read_csv(base_dir/'train.csv')
+    meta_data = pd.read_csv(base_dir / 'train.csv')
     img_dir = base_dir / "train_images"
 
-    if not os.path.exists('./data/rough_masks'):
-        os.makedirs('./data/rough_masks')
+    mask_dir = Path('./data/rough_masks')
+    mask_dir.mkdir(parents=True, exist_ok=True)
 
     for _, row in tqdm(meta_data.iterrows(), total=len(meta_data), desc="Processing volumes"):
         image_id = row["id"]
-        if os.path.exists(f"./data/rough_masks/rough_mask_{image_id}.npz"):
+        tif_mask_path = mask_dir / f"rough_mask_{image_id}.tif"
+        if tif_mask_path.exists():
             continue
-        filename = f"{image_id}.tif"
-        img_path = img_dir / filename
+
+        img_path = img_dir / f"{image_id}.tif"
         volume = load_volume(img_path)
         _, rough_mask = smarter_predict(volume)
-        np.savez_compressed(f"./data/rough_masks/rough_mask_{image_id}.npz", rough_mask=rough_mask)
+
+        # Save as TIFF
+        tifffile.imwrite(tif_mask_path, rough_mask.astype(np.uint8))
+
 
 if __name__ == '__main__':
     main()
