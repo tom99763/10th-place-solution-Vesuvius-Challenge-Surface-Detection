4c24a43299c3b89ad1692f21f2f2554522068c69
tom99 <jonbo99763@gmail.com>
2025-11-22 08:39:26 +0800

    update

---
+++ src/nnUNet/nnunetv2/training/nnUNetTrainer/approx_betti_matching_loss.py
@@ -0,0 +1,189 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_topological.nn import WeightedEulerCurve
+from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
+from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss
+from nnunetv2.utilities.helpers import softmax_helper_dim1
+
+class SinkhornDistance(nn.Module):
+    r"""
+    Given two empirical measures each with :math:`P_1` locations
+    :math:`x\in\mathbb{R}^{D_1}` and :math:`P_2` locations :math:`y\in\mathbb{R}^{D_2}`,
+    outputs an approximation of the regularized OT cost for point clouds.
+
+    Args:
+        eps (float): regularization coefficient
+        max_iter (int): maximum number of Sinkhorn iterations
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
+            'mean': the sum of the output will be divided by the number of
+            elements in the output, 'sum': the output will be summed. Default: 'none'
+
+    Shape:
+        - Input: :math:`(N, P_1, D_1)`, :math:`(N, P_2, D_2)`
+        - Output: :math:`(N)` or :math:`()`, depending on `reduction`
+    """
+    def __init__(self, eps, max_iter, reduction='none'):
+        super(SinkhornDistance, self).__init__()
+        self.eps = eps
+        self.max_iter = max_iter
+        self.reduction = reduction
+
+    def forward(self, x, y):
+        # The Sinkhorn algorithm takes as input three variables :
+        C = self._cost_matrix(x, y)  # Wasserstein cost function
+        x_points = x.shape[-2]
+        y_points = y.shape[-2]
+        if x.dim() == 2:
+            batch_size = 1
+        else:
+            batch_size = x.shape[0]
+
+        # both marginals are fixed with equal weights
+        mu = torch.empty(batch_size, x_points, dtype=torch.float,
+                         requires_grad=False).fill_(1.0 / x_points).squeeze()
+        nu = torch.empty(batch_size, y_points, dtype=torch.float,
+                         requires_grad=False).fill_(1.0 / y_points).squeeze()
+
+        u = torch.zeros_like(mu)
+        v = torch.zeros_like(nu)
+        # To check if algorithm terminates because of threshold
+        # or max iterations reached
+        actual_nits = 0
+        # Stopping criterion
+        thresh = 1e-1
+
+        # Sinkhorn iterations
+        for i in range(self.max_iter):
+            u1 = u  # useful to check the update
+            u = self.eps * (torch.log(mu+1e-8) - torch.logsumexp(self.M(C, u, v), dim=-1)) + u
+            v = self.eps * (torch.log(nu+1e-8) - torch.logsumexp(self.M(C, u, v).transpose(-2, -1), dim=-1)) + v
+            err = (u - u1).abs().sum(-1).mean()
+
+            actual_nits += 1
+            if err.item() < thresh:
+                break
+
+        U, V = u, v
+        # Transport plan pi = diag(a)*K*diag(b)
+        pi = torch.exp(self.M(C, U, V))
+        # Sinkhorn distance
+        cost = torch.sum(pi * C, dim=(-2, -1))
+
+        if self.reduction == 'mean':
+            cost = cost.mean()
+        elif self.reduction == 'sum':
+            cost = cost.sum()
+
+        return cost, pi, C
+
+    def M(self, C, u, v):
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / \epsilon$"
+        return (-C + u.unsqueeze(-1) + v.unsqueeze(-2)) / self.eps
+
+    @staticmethod
+    def _cost_matrix(x, y, p=2):
+        "Returns the matrix of $|x_i-y_j|^p$."
+        x_col = x.unsqueeze(-2)
+        y_lin = y.unsqueeze(-3)
+        C = torch.sum((torch.abs(x_col - y_lin)) ** p, -1)
+        return C
+
+    @staticmethod
+    def ave(u, u1, tau):
+        "Barycenter subroutine, used by kinetic acceleration through extrapolation."
+        return tau * u + (1 - tau) * u1
+
+
+class ApproxBettiMatchingLoss(nn.Module):
+    """
+    Approximate Betti-matching loss for 2D multi-channel or 3D volumes.
+    Input: (B,C,H,W) or (B,D,H,W)
+    """
+    def __init__(self, eps = 0.1, max_iter = 100):
+        super().__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.euler_curve = WeightedEulerCurve()
+        self.wdist = SinkhornDistance(eps=eps, max_iter=max_iter, reduction=None)
+
+    def _compute_summary(self, x):
+        euler = self.euler_curve(x)
+        return euler
+
+    def forward(self, pred, gt, loss_mask=None):
+        """
+        pred, gt: (B,C,H,W) or (B,D,H,W)
+        Returns scalar loss
+        """
+        pred = pred.to(self.device) * loss_mask
+        gt   = gt.to(self.device)
+        B = pred.shape[0]
+
+        betti_loss = torch.tensor(0., device=self.device)
+        for b in range(B):
+            summary_pred = self._compute_summary(pred[b])
+            summary_gt   = self._compute_summary(gt[b])
+            loss_b, P, C = self.wdist(summary_pred, summary_gt)
+            betti_loss += loss_b
+        betti_loss/=B
+        return betti_loss
+
+
+
+class BettiDicCELosss(nn.Module):
+    def __init__(
+        self,
+        soft_dice_kwargs,
+        betti_kwargs,
+        ce_kwargs,
+        weight_ce=1,
+        weight_dice=1,
+        weight_betti=1,
+        ignore_label=None,
+        dice_class=MemoryEfficientSoftDiceLoss,
+    ):
+        super().__init__()
+        if ignore_label is not None:
+            ce_kwargs["ignore_index"] = ignore_label
+
+        self.weight_dice = weight_dice
+        self.weight_ce = weight_ce
+        self.weight_betti = weight_betti
+        self.ignore_label = ignore_label
+
+        self.ce = RobustCrossEntropyLoss(**ce_kwargs)
+        self.dc = dice_class(apply_nonlin=softmax_helper_dim1, **soft_dice_kwargs)
+        self.betti = ApproxBettiMatchingLoss(**betti_kwargs)
+
+    def forward(self, net_output: torch.Tensor, target: torch.Tensor):
+        """
+        target must be b, c, x, y(, z) with c=1
+        :param net_output:
+        :param target:
+        :return:
+        """
+
+        if self.ignore_label is not None:
+            assert target.shape[1] == 1, (
+                "ignore label is not implemented for one hot encoded target variables " "(DC_and_CE_loss)"
+            )
+            mask = target != self.ignore_label
+            # remove ignore label from target, replace with one of the known labels. It doesn't matter because we
+            # ignore gradients in those areas anyway
+            target_dice = torch.where(mask, target, 0)
+            num_fg = mask.sum()
+        else:
+            target_dice = target
+            mask = None
+
+        dc_loss = self.dc(net_output, target_dice, loss_mask=mask) if self.weight_betti != 0 else 0
+        betti_loss = self.betti(net_output, target_dice, loss_mask=mask) if self.weight_betti != 0 else 0
+        ce_loss = (
+            self.ce(net_output, target[:, 0])
+            if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0)
+            else 0
+        )
+        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss + self.weight_srec * betti_loss
+        return result
