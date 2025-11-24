332469831670b12cf3f7f0ca8349bda413a8d791
dimas <znch3142@gmail.com>
2025-11-24 21:18:41 +0800

    update

--- generate_nnunet_soft_oof.py
+++ generate_nnunet_soft_oof.py
@@ -1,30 +1,22 @@
-import json
-from pathlib import Path
-import numpy as np
-import torch
-from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
-from tqdm import tqdm
+import subprocess
+import os
 
-base = Path(f"/path/to/nnunetv2/RESULTS_FOLDER/nnUNet/{cfg}/Dataset{dataset_id}")
+os.environ["nnUNet_raw"] = "./nnunet/nnUNet_raw_data_base/nnUNet_raw"
+os.environ["nnUNet_preprocessed"] = "./nnunet/preprocessed"
+os.environ["nnUNet_results"] = "./nnunet/nnUNet_results"
 
-# load fold splits
-with open(f"./nnunet/preprocessed/Dataset900_VesuviusScroll/splits_final.json") as f:
-    splits = json.load(f)
 
-for fold, split in enumerate(splits):
-    val_cases = split['val']
-    predictor = nnUNetPredictor(tile_step_size=0.5, use_mirroring=True)
 
-    predictor.initialize_from_trained_model_folder(
-        base / f"fold_{fold}",
-        use_folds=(fold,),  # VERY IMPORTANT: only this fold
-    )
+# command as a list of arguments
+cmd = [
+    "nnUNetv2_predict",
+    "-i", "./nnunet/nnUNet_raw_data_base/nnUNet_raw/Dataset900_VesuviusScroll/imagesTr",
+    "-o", "./nnunet/nnUNet_results/Dataset900_VesuviusScroll/nnUNetTrainer__nnUNetResEncUNetMPlans__3d_fullres/oof_softmax",
+    "-d", "900",
+    "-c", "3d_fullres",
+    "-p", "nnUNetResEncUNetMPlans",
+    "--save_probabilities"
+]
 
-    for case_name in val_cases:
-        pred, _ = predictor.predict_from_files(
-            {"image": f"./nnunet/nnUNet_raw_data_base/nnUNet_raw/Dataset900_VesuviusScroll/imagesTr/{case_name}_0000.nii.gz"},
-            return_softmax=True,
-            save_probabilities=False,
-        )
-
-        np.save(f"oof_softmax/{case_name}_soft_fold{fold}.npy", pred)
+# run the command
+result = subprocess.run(cmd, check=True)
