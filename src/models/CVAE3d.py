e5ffc100cfc37069fe6e9fe1537dce3788edcc02
tom99 <jonbo99763@gmail.com>
2025-11-30 00:08:26 +0800

    update

---
+++ src/models/CVAE3d.py
@@ -0,0 +1,8 @@
+'''Reference
+* Park, S., & Kim, H. (2021). Facevae: Generation of a 3d geometric object using variational autoencoders. Electronics, 10(22), 2792.
+* https://github.com/agrija9/Convolutional-VAE-for-3D-Turbulence-Data
+* https://github.com/IsaacGuan/3D-VAE
+* https://github.com/Spartey/3D-VAE-GAN-Deep-Learning-Project (*****)
+'''
+
+#ToDo: Coarse-to-Fine Progressive 3D VAEGAN
