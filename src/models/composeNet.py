bc2a87749739a0e33b3d9a7df5b2a213c942a3b0
tom99763 <jonbo99763@gmail.com>
2025-12-18 09:57:10 +0800

    update

--- src/models/composeNet.py
+++ src/models/composeNet.py
@@ -8,12 +8,17 @@ class ComposeNet3D(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.predictor = instantiate(cfg.models)
+        self.eps = cfg.eps
 
     def forward(self, x):
         prediction = self.predictor(x)
+        prediction = torch.tanh(prediction)
         if not self.training:
             mask = x[:, 1:2]
-            corr = torch.sign(prediction)
+            corr = torch.where(
+                prediction > self.eps, 1,
+                torch.where(prediction < -self.eps, -1, 0)
+            )
             mask = (mask + corr).clamp(0, 1)
             return mask
         return prediction
