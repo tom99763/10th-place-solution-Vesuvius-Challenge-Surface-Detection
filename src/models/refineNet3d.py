48c4c854c863d94e9bd147d7680c24cfe2c04a72
dimas <znch3142@gmail.com>
2026-01-24 07:31:09 +0800

    update

--- src/models/refineNet3d.py
+++ src/models/refineNet3d.py
@@ -10,9 +10,9 @@ from src.models.custom_architecture import CustomUNet
 def create_residual_unet(
         in_channels=2,
         out_channels=3,
-        channels=(16, 32, 64, 128, 160, 160),
-        strides=(1, 2, 2, 2, 2, 2),
-        n_blocks_per_stage=(1, 2, 2, 3, 3, 3),
+        channels=(16, 32, 64, 128),
+        strides=(1, 2, 2, 2),
+        n_blocks_per_stage=(1, 2, 2, 2),
         deep_supervision=False,
 ):
     n_conv_per_stage_decoder = [1] * (len(channels) - 1)
@@ -36,6 +36,8 @@ def create_residual_unet(
         deep_supervision=deep_supervision,
     )
     return model
+
+
 
 class RefineDynUnet(nn.Module):
     def __init__(self, cfg):
